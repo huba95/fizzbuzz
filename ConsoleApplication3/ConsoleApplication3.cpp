6c57c65f009a96f6beae10be025e3171746534c3
Huttman Balázs <huba95@hotmail.com>
2020-08-04 21:50:51 +0200

    Other, more compact version

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -5,25 +5,23 @@ int main()
 {
 	for (int i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-		std::cout << "fizzbuzz";
+			if (i % 3 == 0)
+			{
+				std::cout << "fizz";
+			}
+			if (i % 5 == 0)
+			{
+				std::cout << "buzz";
+			}
 		}
-		else if (i % 3 == 0)
-		{
-			std::cout << "fizz";
-		}
-		else if (i % 5 == 0)
-		{
-			std::cout << "buzz" ;
-		}
-	
 		else
 		{
 			std::cout << i;
 		}
 		std::cout << std::endl;
-}
+	}
 	return 0;
 }
 
